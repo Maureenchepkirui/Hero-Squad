b99516bbc2b35d5694d817f95d5a744685221e2e
maureenchepkirui <maureenchepkirui005@gmail.com>
2020-07-06 16:44:11 +0300

    Test various instances in the Hero class

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -5,39 +5,45 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class HeroTest {
-
+//<..!..........here iam testing if hero instantiates correctly.......!..................>
     @Test
     public void newHero_instantiatesCorrectly_true() {
-        Hero newHero = Hero.setUpNewHero();
-        assertTrue(newHero instanceof Hero);
+        Hero myHero = Hero.setUpNewHero();
+        assertTrue(myHero instanceof Hero);
     }
+    //<......!testing the getName function if it returns the name..........>
     @Test
     public void newHero_getName_String() {
         Hero newHero = Hero.setUpNewHero();
-        assertEquals("Arnold",newHero.getName());
+        assertEquals("Henry",newHero.getName());
     }
-    @Test
-    public void newHero_getAge_Int() {
-        Hero newHero = Hero.setUpNewHero();
-        assertEquals(23,newHero.getAge());
-    }
+   //<...............here am testing if the getPower function returns power......>
     @Test
     public void newHero_getPower_String() {
         Hero newHero = Hero.setUpNewHero();
-        assertEquals("flying",newHero.getPower());
+        assertEquals("Magic",newHero.getPower());
     }
+    //.....<!.........testing if getAge returns Age........................>
+    @Test
+    public void newHero_getAge_Int() {
+        Hero newHero = Hero.setUpNewHero();
+        assertEquals(73,newHero.getAge());
+    }
+    //<..............testing if weakness is returned by the getWeakness function.....>
     @Test
     public void newHero_getWeakness_String() {
         Hero newHero = Hero.setUpNewHero();
-        assertEquals("fire",newHero.getWeakness());
+        assertEquals("Girls",newHero.getWeakness());
     }
+    //<..........!....testing all instances are returned...........
     @Test
     public void newHero_getAllInstances_true() {
-        Hero newHero = Hero.setUpNewHero();
-        Hero another = Hero.setUpNewHero();
-        assertTrue(Hero.getAllInstances().contains(newHero));
-        assertTrue(Hero.getAllInstances().contains(another));
+        Hero myHero = Hero.setUpNewHero();
+        Hero anotherHero = Hero.setUpNewHero();
+        assertTrue(Hero.getAllInstances().contains(myHero));
+        assertTrue(Hero.getAllInstances().contains(anotherHero));
     }
+    //<...........testing for the correct id........
     @Test
     public void newHero_getId_Int() {
         Hero.clearAllHeroes();
@@ -46,6 +52,7 @@ public class HeroTest {
         Hero another1 = Hero.setUpNewHero();
         assertEquals(3,another1.getId());
     }
+    //...................testing if a new hero displays the correct id.............>
     @Test
     public void newHero_findById_id() {
         Hero.clearAllHeroes();
